f1abd9c3d2475b32c06c7948192dff0f19a625ee
Júlia Silvestre <juliasilvestreribeiro2004@gmail.com>
2023-10-20 18:48:44 -0300

    estudando tupla

---
+++ Aula 17 - tupla.py
@@ -0,0 +1,13 @@
+# Uma tupla em Python é semelhante a uma lista.
+# A diferença entre os dois é que não podemos alterar
+# os elementos de uma tupla depois de atribuída,
+# enquanto podemos alterar os elementos de uma lista.
+
+linguagens = ("Python","Ruby","Javascript","Perl","Haskell")
+print(linguagens)
+
+print(linguagens[0:2])
+print(linguagens[-1])
+print(linguagens[:-2])
+print(linguagens[:])
+print(len(linguagens))
